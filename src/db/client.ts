3d5293f306cbf97da314c2d745861b09bd5df73a
Himanshu Gunwant <hgunwant2312@gmail.com>
2026-07-04 15:12:53 +0530

    feat: add expo-sqlite drizzle client

---
+++ src/db/client.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { openDatabaseSync } from 'expo-sqlite';
+
+import * as schema from './schema';
+
+export const DATABASE_NAME = 'finance.db';
+
+const expoDb = openDatabaseSync(DATABASE_NAME, { enableChangeListener: false });
+
+export const db = drizzle(expoDb, { schema });
